031c2f8b4c0deb6ab4c31c491218065e9a28cf95
Natalie Barba <nbarba2010@gmail.com>
2021-09-09 14:55:42 -0700

    commit tree 5662f8ae21ad775a2b2e265228568eaff5a863cc

---
+++ src/components/NavBar.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const linkStyles = {
+    display: "inline-block",
+    width: "50px",
+    padding: "12px",
+    margin: "0 6px 6px",
+    background: "blue",
+    textDecoration: "none",
+    color: "white"
+}
+
+function NavBar() {
+    return (
+      <div>
+        <NavLink exact to="/" style={linkStyles} activeStyle={{background: "darkblue"}}>
+          Home
+        </NavLink>
+        <NavLink exact to="/about" style={linkStyles} activeStyle={{background: "darkblue"}}>
+          About
+        </NavLink>
+        <NavLink exact to="/login" style={linkStyles} activeStyle={{background: "darkblue"}}>
+          Login
+        </NavLink>
+      </div>
+    );
+}
+
+export default NavBar;
